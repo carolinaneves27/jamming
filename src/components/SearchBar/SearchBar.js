7d8c31debadbdd31f77033cca91901de3f647d2f
Carolina Neves <carolinaneves@Carolinas-MacBook-Pro.local>
2018-06-02 13:33:49 +0100

    First commit

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return(
+        <div className="SearchBar">
+            <input placeholder="Enter A Song, Album, or Artist" />
+            <a>SEARCH</a>
+        </div>
+        )}
+}
+
+export default SearchBar;
---
+++ src/components/Track/Track.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Track extends Component{
+    renderAction() {
+        if(isRemovval.ok){
+            
+        }
+    }
+    render() {
+        return(
+            <div className="Track">
+            <div className="Track-information">
+                <h3><!-- track name will go here --></h3>
+                <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+            </div>
+            <a className="Track-action"><!-- + or - will go here --></a>
+            </div>
+        )
+    }
+};
+
+
+export default Track;
